d13f8c9977977a5bb0ade819e2c12f86bf3cbe9a
Kride <krister.landen@gmail.com>
2012-08-20 21:24:07 +0300

    Project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lukkari::Application.config.secret_token = '697c6f19d80e0590603e04afdbb6d9570ea0de1bdedb05720ae6da042166873f28059eda5bcea95628cde2c0190cc0d3d6f6004f5057531e318e4968c1318c9a'
